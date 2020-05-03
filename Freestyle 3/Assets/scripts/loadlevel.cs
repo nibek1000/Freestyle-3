93d59efec52f24ced8426bf119b2e00f37e9e1f2
nibek1000 <nibek1000@gmail.com>
2020-05-03 17:19:44 +0200

    3.0v

---
+++ Freestyle 3/Assets/scripts/loadlevel.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class loadlevel : MonoBehaviour {
+
+    public Button playBut;
+    public InputField st;
+    public InputField nd;
+    public InputField rd;
+    public int number = 0;
+
+    public Color[] m_Colors;
+    private Color currentColour;
+    private int colorIndex = 0;
+
+
+    private void Start()
+    {
+        if (m_Colors.Length > 0)
+        {
+            currentColour = m_Colors[0];
+        }
+    }
+
+    private void Update()
+    {
+        if(int.TryParse(st.text, out number) && int.TryParse(nd.text, out number) && int.TryParse(rd.text, out number))
+        {
+            playBut.interactable = true;
+        }
+        else
+        {
+            playBut.interactable = false;
+        }
+
+        for (int i = 0; i < m_Colors.Length; i++)
+        {
+            // Get the currentColor in the Array
+            if (currentColour == m_Colors[i])
+            {
+                colorIndex = i + 1 == m_Colors.Length ? 0 : i + 1;
+            }
+        }
+        Color nextColor = m_Colors[colorIndex];
+        // Lerp Color _>
+        currentColour = Color.Lerp(currentColour, nextColor, 0.01f);
+        Camera.main.backgroundColor = currentColour;
+    }
+
+
+    public void LoadLevel()
+    {
+        PlayerPrefs.SetFloat("st", float.Parse(st.text));
+        PlayerPrefs.SetInt("nd", int.Parse(nd.text));
+        PlayerPrefs.SetInt("rd", int.Parse(rd.text));
+        Application.LoadLevel(1);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void License()
+    {
+        Application.OpenURL("https://pixabay.com/pl/service/license/");
+    }
+}
---
+++ Freestyle 3/Assets/scripts/main.cs
@@ -0,0 +1,209 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class main : MonoBehaviour {
+    public Sprite[] imagesToRoll;
+    public Image nowImage;
+    public Image nextImage;
+    private int next;
+    private int now;
+    public Slider sliderTime;
+    public Text changenarrator;
+    private int changeIn;
+    public GameObject changeGO;
+    public GameObject fail;
+    public Text scoreText;
+    private int score;
+    public Text EndTimer;
+    private int tillEnd;
+
+    public GameObject finished;
+    public Text totalScore;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(firstRoll());
+        Debug.Log("Game started");
+
+        if(PlayerPrefs.GetInt("rd") == 0)
+        {
+            tillEnd = 999999999;
+        }
+        else
+        {
+            tillEnd = PlayerPrefs.GetInt("rd");
+        }
+
+
+        if (PlayerPrefs.GetInt("nd") == 0)
+        {
+            changeIn = 999999999;
+        }
+        else
+        {
+            changeIn = PlayerPrefs.GetInt("nd");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (PlayerPrefs.GetInt("rd") == 0)
+        {
+            EndTimer.text = "Till story ending: " + "∞";
+        }
+        else
+        {
+            EndTimer.text = "Till story ending: " + tillEnd;
+        }
+
+
+        if (PlayerPrefs.GetInt("nd") == 0)
+        {
+            changenarrator.text = "Change narrator in: " + "∞";
+        }
+        else
+        {
+            changenarrator.text = "Change narrator in: " + changeIn;
+        }
+
+
+	}
+    public void SomeoneFailed()
+    {
+        StopAllCoroutines();
+        fail.SetActive(true);
+        scoreText.text = "You made: " + score + " in row!";
+        StartCoroutine(finish());
+    }
+
+    void FinishedGame()
+    {
+        StopAllCoroutines();
+        finished.SetActive(true);
+        totalScore.text = "You made: " + score + " in row!";
+        StartCoroutine(finish());
+    }
+
+    IEnumerator finish()
+    {
+        yield return new WaitForSeconds(5);
+        Application.LoadLevel(0);
+    }
+
+
+    IEnumerator firstRoll()
+    {
+
+        Debug.Log(PlayerPrefs.GetFloat("st") / 100);
+        yield return new WaitForSeconds(PlayerPrefs.GetFloat("st") / 100);
+        sliderTime.value = sliderTime.value - 0.01f;
+        if (sliderTime.value == 0)
+        {
+            now = Random.Range(0, 249);
+            next = Random.Range(0, 249);
+            Apply();
+            Debug.Log("first roll ended");
+            sliderTime.value = 1;
+            StartCoroutine(roll());
+        }
+        else
+        {
+            StartCoroutine(firstRoll());
+        }
+        
+    }
+
+    IEnumerator roll()
+    {
+        Debug.Log("roll started");
+        yield return new WaitForSeconds(PlayerPrefs.GetFloat("st") / 100);
+        sliderTime.value = sliderTime.value - 0.01f;
+        if (sliderTime.value == 0)
+        {
+
+
+            if (changeIn == 0)
+            {
+                sliderTime.value = 1;
+                StartCoroutine(changei());
+
+            }
+            else
+            {
+                StartCoroutine(nochange());
+
+            }
+
+        }
+        else
+        {
+            StartCoroutine(roll());
+        }
+    }
+
+
+    IEnumerator nochange()
+    {
+        yield return new WaitForSeconds(PlayerPrefs.GetFloat("st") / 100);
+        sliderTime.value = sliderTime.value - 0.01f;
+        if (sliderTime.value == 0)
+        {
+            changeIn--;
+            tillEnd--;
+            changeGO.SetActive(false);
+            now = next;
+            score++;
+            next = Random.Range(0, 249);
+            Apply();
+            Debug.Log("DONE!");
+            sliderTime.value = 1;
+            StartCoroutine(roll());
+            if(tillEnd == 0)
+            {
+                FinishedGame();
+            }
+
+        }
+        else
+        {
+            StartCoroutine(nochange());
+        }
+    }
+
+
+    IEnumerator changei()
+    {
+        changeGO.SetActive(true);
+        yield return new WaitForSeconds(PlayerPrefs.GetFloat("st") / 100);
+        sliderTime.value = sliderTime.value - 0.01f;
+        if (sliderTime.value == 0)
+        {
+            changeIn = PlayerPrefs.GetInt("nd");
+            changeGO.SetActive(false);
+            now = next;
+            score++;
+            next = Random.Range(0, 249);
+            Apply();
+            Debug.Log("DONE!");
+            sliderTime.value = 1;
+            StartCoroutine(roll());
+            if (tillEnd == 0)
+            {
+                FinishedGame();
+            }
+        }
+        else
+        {
+            StartCoroutine(changei());
+        }
+    }
+
+
+    void Apply()
+    {
+        nowImage.GetComponent<Image>().sprite = imagesToRoll[now];
+        nextImage.GetComponent<Image>().sprite = imagesToRoll[next];
+    }
+}
